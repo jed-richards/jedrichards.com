0dc15fcfd5a4fa07c0abca7ed632a50743e15caa
jed-richards <jed22richards@gmail.com>
2025-08-13 11:54:58 -0500

    feat: add nix flake for dev shell

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Flake for my personal website and blog";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+
+  outputs = { self, nixpkgs, }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
+      pkgsForSystem = system: (import nixpkgs { inherit system; });
+
+    in {
+      devShells = forAllSystems (system:
+        let pkgs = pkgsForSystem system;
+        in {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.nodePackages_latest.prettier
+              pkgs.pnpm
+              pkgs.typescript
+              pkgs.typescript-language-server
+            ];
+          };
+        });
+    };
+}
