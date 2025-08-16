aea2636bcc3754d04e548c90e538e9b78cda4a3c
jed-richards <jed22richards@gmail.com>
2025-08-16 15:48:44 -0500

    deps: add prettier and prettier-plugin-astro
    This removes prettier from the nix flake and rather adds it as a dev
    dependency with `pnpm add --save-dev --save-exact prettier
    prettier-plugin-astro`.

--- flake.nix
+++ flake.nix
@@ -1,29 +1,36 @@
 {
   description = "Flake for my personal website and blog";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
 
-  outputs = { self, nixpkgs, }:
+  outputs =
+    { self, nixpkgs }:
     let
       forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
       pkgsForSystem = system: (import nixpkgs { inherit system; });
 
-    in {
-      devShells = forAllSystems (system:
-        let pkgs = pkgsForSystem system;
-        in {
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = pkgsForSystem system;
+        in
+        {
           default = pkgs.mkShell {
             packages = [
               # (pkgs.nodePackages_latest.nodejs.overrideAttrs
               #   (final: prev: { enableNpm = false; })) # NodeJS without NPM
               pkgs.nodePackages_latest.nodejs
-              pkgs.nodePackages_latest.prettier
               pkgs.pnpm
               pkgs.typescript
               pkgs.typescript-language-server
               pkgs.astro-language-server
             ];
           };
-        });
+        }
+      );
     };
 }
